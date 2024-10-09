78089748a31638d948bf0db3104d20ccf8916d20
ocmodi21 <ocmodi2117@gmail.com>
2024-10-09 19:43:23 +0530

    chore: design schema for coins data

---
+++ src/models/coin.model.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Coin extends Document {
+  symbol: string;
+  price: number;
+  marketCap: number;
+  "24hChange": number;
+}
+
+const CoinSchema: Schema = new Schema(
+  {
+    symbol: { type: String, required: true, unique: true },
+    price: { type: Number, required: true },
+    marketCap: { type: Number, required: true },
+    "24hChange": { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const CoinModel = mongoose.model<Coin>("Coin", CoinSchema);
+
+export default CoinModel;
