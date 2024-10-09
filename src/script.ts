c5d06016eddbfcefdfd44e093cbebc8791da8772
ocmodi21 <ocmodi2117@gmail.com>
2024-10-09 20:23:30 +0530

    refactor: script file

--- src/script.ts
+++ src/script.ts
@@ -2,20 +2,16 @@ import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
 
-interface CoinIds {
-  id: string;
-}
-
 async function fetchCoinIDs(): Promise<void> {
   const url = (process.env.BASE_URL as string) + "/list";
-  const coinIds: CoinIds[] = [];
+  const coinIds: string[] = [];
 
   try {
     const response = await fetch(url);
     const data = await response.json();
 
     data.forEach((coin: { id: string }) => {
-      coinIds.push({ id: coin.id });
+      coinIds.push(coin.id);
     });
 
     // Convert the coinIds array to JSON format
